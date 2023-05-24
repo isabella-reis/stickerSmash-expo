6de36a3dfc67ec8f6125499d6452bbd1fb8651da
Isabella Reis <isabellanery15@hotmail.com>
2023-05-24 14:17:48 -0300

    Creating the screen - button + image

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import ImageViewer from "./components/ImageViewer";
+import Button from "./components/Button";
+
+const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageViewer placeholderImageSource={PlaceholderImage}/>
+      <Text style={{ color: "#fff" }}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Button label={'Choose a photo'} theme="primary"/>
+      <Button label={'Use this photo'}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +22,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footerContainer: {
+    flex: 1/3,
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
