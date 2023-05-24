d47f2b658f032fd9493e88790daa2000a9bc3fd0
Isabella Reis <isabellanery15@hotmail.com>
2023-05-24 14:45:29 -0300

    Adding functionality to pick image from gallery.

--- App.js
+++ App.js
@@ -1,19 +1,37 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 import ImageViewer from "./components/ImageViewer";
 import Button from "./components/Button";
+import { useState } from "react";
 
 const PlaceholderImage = require("./assets/images/background-image.png");
 
 export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const pickImageAsync = async () => {
+    // We receive an object in which different options are specified.
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true, //user can crop images when on android and iOS
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ImageViewer placeholderImageSource={PlaceholderImage}/>
-      <Text style={{ color: "#fff" }}>
-        Open up App.js to start working on your app!
-      </Text>
-      <Button label={'Choose a photo'} theme="primary"/>
-      <Button label={'Use this photo'}/>
+      <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
+      <Button
+        label={"Choose a photo"}
+        theme="primary"
+        onPress={pickImageAsync}
+      />
+      <Button label={"Use this photo"} />
       <StatusBar style="auto" />
     </View>
   );
@@ -27,7 +45,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   footerContainer: {
-    flex: 1/3,
-    alignItems: 'center',
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
